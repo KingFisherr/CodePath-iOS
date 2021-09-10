333643fa72a76ed29a157545a84d0808893466fe
KingFisherr <tahsinprovath@aol.com>
2021-09-10 19:45:39 -0400

    Flix App 1.0 Added connection with API...

---
+++ Flix/flixter/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Tahsin Provath on 9/10/21.
+//  Copyright © 2021 Tahsin Provath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
